1080c43ecf7899e4a5e6d7160ae25548c3aab382
kv.rodriguezgarcia@gmail.com <kv.rodriguezgarcia@gmail.com>
2024-03-13 15:00:35 -0300

    Actualizacion

--- PruebaAtentoPython/aplicacionSelenium.py
+++ PruebaAtentoPython/aplicacionSelenium.py
@@ -10,11 +10,4 @@ path_driver=os.chdir('C:\driver')
 driver=webdriver.Chrome(executable_path='chromedriver')
 time.sleep(10)"""
 
-class Store_Sele(webdriver.Chrome):
-    def __init__(self, driver_path='C:\driver\chromedriver', teardown=False):
-        self.driver_path=driver_path
-        self.teardown=teardown
-
-        super(Store_Sele,self).__init__()
-        self.maximize_window
 
