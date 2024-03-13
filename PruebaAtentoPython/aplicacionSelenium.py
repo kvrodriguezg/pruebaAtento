b8909d267465ef2049c0431404e908ccf022ff3c
kv.rodriguezgarcia@gmail.com <kv.rodriguezgarcia@gmail.com>
2024-03-13 16:59:25 -0300

    Actualizacion Selenium

--- PruebaAtentoPython/aplicacionSelenium.py
+++ PruebaAtentoPython/aplicacionSelenium.py
@@ -2,12 +2,35 @@ import selenium
 import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-#from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
-"""url = ''
-path_driver=os.chdir('C:\driver')
-driver=webdriver.Chrome(executable_path='chromedriver')
-time.sleep(10)"""
+url = 'https://www.falabella.com/falabella-cl'
 
+
+service = Service(executable_path=r'C:\driver\chromedriver.exe')
+
+
+driver = webdriver.Chrome(service=service)
+
+driver.get(url)
+
+
+botoncerrar = driver.find_element(By.XPATH,'//div[@class="dy-lb-close"]')
+botoncerrar.click()
+
+
+busqueda = driver.find_element(By.XPATH,'//input[@id="testId-SearchBar-Input"]')
+busqueda.send_keys('prueba')
+botonBuscar = driver.find_element(By.XPATH,'//button[@class="SearchBar-module_searchBtnIcon__2L2s0"]')
+botonBuscar.click()
+
+time.sleep(60)
+
+
+
+
+driver.quit()
 
